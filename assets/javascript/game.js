f968e24673e270b1851d3d9f237ddc359faec362
Anton Mykhaylov <amykhaylov@Antons-MacBook-Pro.local>
2017-11-30 10:49:45 -0600

    release 1.1

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// variable to hold number of wins, loses, guesses left, and guess made so far by the user
+var wins = 0;
+var losses = 0;
+var guessesLeft = 0;
+var yourGuessesSoFar = 0;
+
+// Function that runs whenever a user presses a key
+document.onkeyup = function(event) {
+
+}
