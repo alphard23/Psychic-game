02e444ae390aa44df3ba86b1b82466bc80a3876f
Anton Mykhaylov <amykhaylov@antons-mbp.lan>
2017-12-05 23:59:32 -0600

    ff2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,9 +15,9 @@ var letterToGuess = null;
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 //Allows the user 9 guesses
-// guesses = guesses || 9
+
 var updateGuessesLeft = function() {
-    // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+    // Grabbing the HTML element and setting it equal to the guessesLeft
     document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
@@ -25,7 +25,7 @@ var updateLetterToGuess = function() {
     this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
 };
 var updateGuessesSoFar = function() {
-    // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+    // Take the guesses the user has tried -- then display it as letters separated by commas. 
     document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
 // Function will be called when we reset everything
@@ -60,11 +60,11 @@ document.onkeyup = function(event) {
             reset();
         }
     } else if (guessesLeft == 0) {
-        // Then we will loss and we'll update the html to display the loss 
+        // Upon losing update the html to display the loss 
         losses++;
         document.querySelector('#losses').innerHTML = "Losses: " + losses;
         alert("Sorry, you're not psychic, maybe try again?");
-        // Then we'll call the reset. 
+        // Then call the reset. 
         reset();
     }
 };
